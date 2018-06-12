a09bc22b394f5d187cefcaa86ed45630da1f3514
oldmonitor <dho5354@hotmail.com>
2018-06-12 01:09:00 -0400

    updated servoce

---
+++ Server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+var bc Blockchain
+
+//Server for handling web request
+type Server struct {
+	httpPort string
+}
+
+func (s Server) run() error {
+	mux := s.makeMuxRouter()
+
+	bc.addBlock([]byte("new transaction 1"))
+	log.Println("Listening on ", s.httpPort)
+	ser := &http.Server{
+		Addr:           ":" + s.httpPort,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := ser.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s Server) makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", s.handleGetBlockchain).Methods("GET")
+	//muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
+}
+
+func (s Server) handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	/*bytes, err := json.MarshalIndent(bc, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}*/
+	io.WriteString(w, bc.Chain[0].toString())
+}
--- main.go
+++ main.go
@@ -1,61 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
-
-var bc Blockchain
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(os.Getenv("HTTP_PORT"))
-	run()
-}
 
-func run() error {
-
-	mux := makeMuxRouter()
-	httpAddr := os.Getenv("HTTP_PORT")
-	log.Println("Listening on ", os.Getenv("HTTP_PORT"))
-	s := &http.Server{
-		Addr:           ":" + httpAddr,
-		Handler:        mux,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	if err := s.ListenAndServe(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func makeMuxRouter() http.Handler {
-	muxRouter := mux.NewRouter()
-	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
-	//muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
-	return muxRouter
-}
-
-func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
-	bytes, err := json.MarshalIndent(bc, "", "  ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	io.WriteString(w, string(bytes))
+	var s Server
+	s.httpPort = os.Getenv("HTTP_PORT")
+	s.run()
 }
